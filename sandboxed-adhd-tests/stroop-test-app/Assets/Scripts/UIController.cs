dbaf13f4513a9341d6d7d66098329b0980cf28c2
Dominik Szczepek <DSzczepek@outlook.de>
2023-12-28 21:43:46 +0100

    The color word can change now

--- sandboxed-adhd-tests/stroop-test-app/Assets/Scripts/UIController.cs
+++ sandboxed-adhd-tests/stroop-test-app/Assets/Scripts/UIController.cs
@@ -1,33 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = System.Random;
 
 public class UIController : MonoBehaviour
 {
+    VisualElement root;
+
+    VisualElement veLabelContainer;
+    Label lbColorWord;
+
+    VisualElement veButtonContainer;
+    Button btGreen;
+    Button btRed;
+    Button btBlue;
+    Button btYellow;
+
+    enum ColorWordColor {
+        WHITE,
+        GREEN,
+        RED,
+        BLUE,
+        YELLOW
+    };
+
+    enum ColorWordText
+    {
+        DOT,
+        GREEN,
+        RED,
+        BLUE,
+        YELLOW
+    };
+
+    ColorWordColor colorWordColor;
+    ColorWordText colorWordText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorWordColor = ColorWordColor.WHITE;
+        colorWordText = ColorWordText.DOT;
+
+        root = GetComponent<UIDocument>().rootVisualElement;
+
+        veLabelContainer = root.Q<VisualElement>("VELabelContainer");
+        lbColorWord = veLabelContainer.Q<Label>("LbColorWord");
+
+        veButtonContainer = root.Q<VisualElement>("VEButtonContainer");
+        btGreen = veButtonContainer.Q<Button>("BtGreen");
+        btRed = veButtonContainer.Q<Button>("BtRed");
+        btBlue = veButtonContainer.Q<Button>("BtBlue");
+        btYellow = veButtonContainer.Q<Button>("BtYellow");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Random randomizer = new Random();
+
+        ChangeColor(randomizer.Next(1, 5));
+        ChangeText(randomizer.Next(1, 5));
+
+        lbColorWord.text = colorWordText.ToString();
+
+        switch(colorWordColor)
+        {
+            case 0:
+                lbColorWord.style.color = new Color(255, 255, 255);
+                break;
+            case (ColorWordColor) 1:
+                lbColorWord.style.color = new Color(0, 255, 0);
+                break;
+            case (ColorWordColor) 2:
+                lbColorWord.style.color = new Color(255, 0, 0);
+                break;
+            case (ColorWordColor) 3:
+                lbColorWord.style.color = new Color(0, 0, 255);
+                break;
+            case (ColorWordColor) 4:
+                lbColorWord.style.color = new Color(255, 255, 0);
+                break;
+        }
+
     }
 
-    private void OnEnable()
+    private void ChangeColor(int colorNr)
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        this.colorWordColor = (ColorWordColor)colorNr;
+    }
 
-        VisualElement veLabelContainer = root.Q<VisualElement>("VELabelContainer");
-        Label lbColorWord = veLabelContainer.Q<Label>("LbColorWord");
 
-        VisualElement veButtonContainer = root.Q<VisualElement>("VEButtonContainer");
-        Button btGreen = veButtonContainer.Q<Button>("BtGreen");
-        Button btRed = veButtonContainer.Q<Button>("BtRed");
-        Button btBlue = veButtonContainer.Q<Button>("BtBlue");
-        Button btYellow = veButtonContainer.Q<Button>("BtYellow");
+    private void ChangeText(int textNr)
+    {
+        this.colorWordText = (ColorWordText)textNr;
     }
 }
